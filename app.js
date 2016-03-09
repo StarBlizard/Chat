efbabd50ae5208fce36ed71d8076a8fb76143655
Narciso Guillen <guillen.narciso@gmail.com>
2016-03-08 19:53:51 -0600

    Add git ignore

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("holi");
+var express = require('express');
+var app     = express();
+var server  = app.listen(3030);
+
+app.get('/', function(req, res){
+  res.status(200).send('Hello');
+});
